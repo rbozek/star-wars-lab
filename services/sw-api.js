cf61684e58f951dde3a47385ee99d6283e7efca5
rbozek <rbozek@gmail.com>
2024-02-04 18:40:32 -0500

    complete part of Step 3, correct errors in api file

--- services/sw-api.js
+++ services/sw-api.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://sei-dnd-api.herokuapp.com'
+const baseUrl = 'https://swapi.dev/api/'
 
 export async function getAllStarships() {
   const res = await fetch(`${baseUrl}/starships`)
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
+// services
+import { getAllStarships } from '../services/sw-api'
+
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [starshipList, setStarshipList] = useState([])
+
+  useEffect(() => {
+    const fetchStarshipList = async () => {
+      // make an API call
+      const starshipData = await getAllStarships()
+      // set state to the result from that call (data!)
+      setStarshipList(starshipData)
+    }
+    fetchStarshipList()
+    // console.log('test '+starshipList);
+  }, [])
+  // console.log('test '+starshipList);
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Test!</h1>
     </>
   )
 }
 
 export default App
+
+{/* <div>
+  <a href="https://vitejs.dev" target="_blank">
+    <img src={viteLogo} className="logo" alt="Vite logo" />
+  </a>
+  <a href="https://react.dev" target="_blank">
+    <img src={reactLogo} className="logo react" alt="React logo" />
+  </a>
+</div>
+<h1>Vite + React</h1>
+<div className="card">
+  <button onClick={() => setCount((count) => count + 1)}>
+    count is {count}
+  </button>
+  <p>
+    Edit <code>src/App.jsx</code> and save to test HMR
+  </p>
+</div>
+<p className="read-the-docs">
+  Click on the Vite and React logos to learn more
+</p> */}
