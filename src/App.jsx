2d701f0263bff474224296c53c6c2b017d5359ff
rbozek <rbozek@gmail.com>
2024-02-05 18:11:33 -0500

    complete step 3, no styling yet

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,13 @@ function App() {
     const fetchStarshipList = async () => {
       // make an API call
       const starshipData = await getAllStarships()
+
       // set state to the result from that call (data!)
-      setStarshipList(starshipData)
+      // DIFFERENT than DND! Data coming back from DB had nested property!! (have to look at data to see it should be .results)
+      setStarshipList(starshipData.results)
     }
     fetchStarshipList()
-    // console.log('test '+starshipList);
+    console.log('test 2nd',starshipList);
   }, [])
   // console.log('test '+starshipList);
 
@@ -24,6 +26,7 @@ function App() {
     <main>
       <h1>Test!</h1>
       {/* {console.log('testing '+starshipList)} */}
+      {/* {Object.entries(starshipList).map(starship =>  */}
       {starshipList.map(starship => 
         <div className="starship-card" key={starship._id}>
           <p>{starship.name}</p>
